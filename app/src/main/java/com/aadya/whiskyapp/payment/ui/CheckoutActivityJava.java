e9cd4bca532f649576bc300d5c6f35eb2eea31aa
kirti <kashyap.kirti11@gmail.com>
2021-10-29 06:49:29 +0100

    create UI for address and find ids

--- app/src/main/java/com/aadya/whiskyapp/payment/ui/CheckoutActivityJava.java
+++ app/src/main/java/com/aadya/whiskyapp/payment/ui/CheckoutActivityJava.java
@@ -3,8 +3,12 @@ package com.aadya.whiskyapp.payment.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
@@ -27,6 +31,7 @@ import com.stripe.android.view.CardMultilineWidget;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -49,11 +54,21 @@ public class CheckoutActivityJava extends AppCompatActivity {
     String amount;
     private TextView amountTextView;
     Button payButton;
+    private  ArrayAdapter<String> noofpeopleAdapter;
+    private ArrayList<String> noOfPeopleList;
+    EditText addressET;
+    Spinner countrySp,stateSp,citySp,postSp;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_java);
+        addressET=findViewById(R.id.tv_address_line);
+        countrySp=findViewById(R.id.country_spinner);
+        stateSp=findViewById(R.id.state_spinner);
+        citySp=findViewById(R.id.city_spinner);
+        postSp=findViewById(R.id.postcode_spinner);
+
         payButton = findViewById(R.id.payButton);
         ImageView imgDrawer= findViewById(R.id.img_drawer);
         ImageView profileIcon= findViewById(R.id.img_logo);
@@ -64,11 +79,31 @@ public class CheckoutActivityJava extends AppCompatActivity {
         amount = intent.getStringExtra("amount");
         amountTextView = findViewById(R.id.amountTextView);
         amountTextView.setText("Total "+amount);
+        amountTextView.setVisibility(View.GONE);
+        LinearLayout addressLayout=findViewById(R.id.address_layout);
+        LinearLayout paymentLayout=findViewById(R.id.payment_layout);
+        payButton.setText("Total "+amount +" CONFIRM ORDER" );
         // Configure the SDK with your Stripe publishable key so it can make requests to Stripe
         stripe = new Stripe(
                 getApplicationContext(),
                 Objects.requireNonNull("pk_test_51Jlt4ySAToMq1oS5dtA0kVj5d5nmZV7pzAaiucGkO2CFPqHuYz22tmHoNKPQaFtbLNDwYZh7lKc9JQRAAv7gHuJT00NRFz273R")
         );
+        Button preButton=findViewById(R.id.prevButton);
+        Button nextButton=findViewById(R.id.nexButton);
+        nextButton.setVisibility(View.GONE);
+        preButton.setVisibility(View.GONE);
+        preButton.setOnClickListener((View view) -> {
+            nextButton.setVisibility(View.VISIBLE);
+            preButton.setVisibility(View.GONE);
+            addressLayout.setVisibility(View.VISIBLE);
+            paymentLayout.setVisibility(View.GONE);
+        });
+        nextButton.setOnClickListener((View view) -> {
+            nextButton.setVisibility(View.GONE);
+            preButton.setVisibility(View.VISIBLE);
+            addressLayout.setVisibility(View.GONE);
+            paymentLayout.setVisibility(View.VISIBLE);
+        });
         Button cancelButton=findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener((View view) -> {
             finish();
